b915664b1ec0df8ac6a4a5998b6cdc51c9c22b3a
arsimr16 <arsimr16@g.holycross.edy>
2017-09-15 21:11:59 -0700

    stubbed out handleRequest

--- web/request-handler.js
+++ web/request-handler.js
@@ -12,13 +12,28 @@ var headers = {
 };
 
 exports.handleRequest = function (req, res) {
+  // how to tell diff between / AND /style.css vs. /www.google.com AND /www.walmart.com
   if (req.method === 'GET') {
+    if ((/\/www./).test(req.url)) {
+      // return archived page
+    } else if ((/\/style.css/).test(req.url)) {
+      // return the css page inside web/public
+    } else if ((/\//).test(req.url)) {
+      //return index.html from web/public
+    }
+    // console.log('LOOOOOOOOOK ATTTTTT MEEEEE', req);
     var baseUrl = archive.paths.siteAssets;
-    var url = baseUrl + req.url + 'index.html';
+    console.log('--------------------------', req.path);
+    var url = baseUrl + req.url + '/index.html';
+    //var archiveUrl = archive.paths.archivedSites + req.url;
+    //console.log('HEEEEEEEEEEEEEEEEEEEEEEEEEEEEEY', url);
     fs.readFile(url, (err, data) => {
+      console.log('url!!!!!!!!!1', url);
+      //console.log('DDDDDDDDDDAAAAAAAAAATTTTTTTTAAAAAAAA', data);
       if (err) {
         res.writeHead(404, headers);
         res.end(data);
+        console.log('ERRRRRRRRRRRRRRRRRRRRRRRORRRRRRRRRRRRRRR', err);
       } else {
         res.writeHead(200, headers);
         res.end(data); 
