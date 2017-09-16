c74e0a87f6dacd1926ff8996e4b8cf6c1983ced2
arsimr16 <arsimr16@g.holycross.edy>
2017-09-16 15:12:53 -0700

    implemented some more helper functions

--- helpers/archive-helpers.js
+++ helpers/archive-helpers.js
@@ -44,18 +44,55 @@ exports.readListOfUrls = function(callback) {
 };
 
 exports.isUrlInList = function(url, callback) {
+  fs.readFile(exports.paths.list, (err, data) => {
+    if (err) {
+      console.log('try again', err);
+    }
+    data = data.toString().split('\n');
+    console.log('check me out i should be an array', data);
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] === url) {
+        return callback(true);
+      } 
+    }
+    return callback(false);
+  });
+  
   //  returns a boolean
   //  if false, run callback (addUrlToList)
   //  if true, run call
 };
 
 exports.addUrlToList = function(url, callback) {
-  //  add newline character 
-  //  add url
+  //  add newline character
+    //  add url
+  fs.appendFile(exports.paths.list, '\n' + url, (err) => {
+    if (err) {
+      console.log('hey try again to add me to file', err);
+    }
+  });
+  callback();
   
 };
 
 exports.isUrlArchived = function(url, callback) {
+  //  find the archives folder
+  fs.readdir(exports.paths.archivedSites, (err, files) => {
+    if (err) {
+      console.log('ohhhhhh nooooooo', err);
+    }
+    console.log('looooooooooooooooooooook', files);
+    for (var i = 0; i < files.length; i++) {
+      if (files[i] === url) {
+        callback(true);
+      }
+    }
+    //  loop through folder and check against url
+      //  if it does, callback(true)
+      //  callback(false)
+    callback(false);
+  });
+  
 };
 
 exports.downloadUrls = function(urls) {
